722a1c9f1f8bedb0fc94c76609afd4be4b897929
zozrom <zozulia.roman135@gmail.com>
2019-01-22 21:02:12 +0200

    First commit!!!

--- Project12/Source.cpp
+++ Project12/Source.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-	return 0;
+	
+
+	//This five - story building, on each floor of which there are various stores.
+	//Write a simulator of the elevator with a floor picker and get information 
+	//	 about the store that is on it
+					 
+	       return 0;
+
    }
