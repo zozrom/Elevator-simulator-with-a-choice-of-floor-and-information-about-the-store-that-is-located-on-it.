86ba155d86611e6f21c98df45ee905ac6d57a304
zozrom <zozulia.roman135@gmail.com>
2019-01-22 21:22:34 +0200

    Two commit!

--- Project12/Source.cpp
+++ Project12/Source.cpp
@@ -6,7 +6,15 @@ int main() {
 	//This five - story building, on each floor of which there are various stores.
 	//Write a simulator of the elevator with a floor picker and get information 
 	//	 about the store that is on it
-					 
+	int n;
+	do
+	{
+		cout << "Enter the floor";
+			cin >> n;
+	} while (n <= 0 || n > 5);
+		enum floor{ One,Two, Three, Four,Five}Shop_Adidas, Shop_Nike, Shop_Reabok,Shops_Puma, Shop_Asics;
+		 
+
 	       return 0;
 
    }
